2b6314fa50473a68b101a9d854585f4b8aff004a
Your Name <you@example.com>
2025-02-14 19:33:52 +0530

    createSplashscreen

---
+++ lib/dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dashboard_screen extends StatelessWidget {
+  const Dashboard_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('hello flutter'),);
+  }
+}
